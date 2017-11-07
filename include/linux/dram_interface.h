835fed5bbc31d2b94c315d99240b1fc831bf434c
liuzix <lzx@uchicago.edu>
2017-11-07 12:59:35 -0600

    Update dram_interface.h

--- include/linux/dram_interface.h
+++ include/linux/dram_interface.h
@@ -4,8 +4,8 @@
 typedef free_chunk_info* (*traverse_func_ptr) (void*, size_t, size_t*);
 
 struct free_chunk_info {
- 		void* start; // pointer to the free chunk
-   	size_t len; // size of the free chunk
+    void* start; // pointer to the free chunk
+    size_t len; // size of the free chunk
 };
 
 void register_traverse(int identifier, traverse_func_ptr traverse); 
